5bcb5e6d86a2ee8dce0d65f11a3392a944191134
Jack <boscoj24@mail.wlu.edu>
2022-11-28 23:29:21 -0500

    draw trees in latex

--- drawTree.py
+++ drawTree.py
@@ -1,5 +1,5 @@
 from typing import List, AnyStr
-
+from numpy import arange
 class PrintNode:
     #returns a list of all children
     def getChildren(self) -> List:
@@ -22,11 +22,12 @@ class drawTree:
         youngerSibling = {head : None}
         level = 0
         while any([n is not None for n in parentLevel]):
-            print(str([(p.getVal(), getTag[p]) for p in parentLevel]), end='')
-            input()
+            #print(str([(p.getVal(), getTag[p]) for p in parentLevel]), end='')
+            #input()
             isFirstInLevel = True
             for p in parentLevel:
                 kids = p.getChildren()
+                
                 for child in range(len(kids)):
                     if kids[child]:
                         findParent[kids[child]] = p
@@ -37,77 +38,56 @@ class drawTree:
                             youngerSibling[kids[child]] = kids[child - 1]
                             getTag[kids[child]] = [getTag[p][0] + 1, child + getTag[kids[child - 1]][1]]
                         childLevel.append(kids[child])
-                        
-                #remove non init DONE
-                #only fist in a level is below DONE
-                #first sibling shifted left 1 cm
-                #edges
-                if findParent[p]:
-                    ret += '\\node[state'
-                    if isFirstInLevel:
-                        isFirstInLevel = False
-                        ret += ', below of=' + str(getTag[findParent[p]][0]) + '-' + str(getTag[findParent[p]][1])
-                        ret += ', xshift=-1cm'
-                    #if youngerSibling[p]:
-                    else:
-                        ret += ', right of=' + str(getTag[p][0]) + '-' + str(getTag[p][1] - 1)
-                        if youngerSibling[p]:
-                            ret += ', xshift=1cm'
-                    ret += '] ('+ str(getTag[p][0]) + '-' + str(getTag[p][1]) + ') {' + str(p.getVal()) + '};\n'
-                else:
-                    ret += '\\node[state, initial] (0-0) {' + head.getVal() + '};\n'
-                    
-            #draw the edges
-            ret += '\\draw'
-            for child in childLevel:
-                if child and findParent[child]:
-                    p = findParent[child]
-                    ret += '(' + str(getTag[p][0]) + '-' + str(getTag[p][1]) + ') \
-                    edge[below] node{$ $} (' + str(getTag[child][0]) + '-' + str(getTag[child][1]) +')\n'
-            ret = ret[:-1] + ';' 
             parentLevel = childLevel.copy()
             childLevel.clear()
             level += 1
+        
+        parentLevel = [head]
+        curLev = 1
+        while any([n is not None for n in parentLevel]):
+            sibNum = 0
+            for p in parentLevel:     
+                if p and findParent[p]:
+                    spaceRange = 2**(level - curLev)
+                    siblings = findParent[p].getChildren()
+                    numSiblings = 1 if len(siblings) == 1 else len(siblings) - 1
+                    sibPos = [i for i in arange(-spaceRange/2.0, spaceRange/2.0+1, spaceRange/(numSiblings))]
+                    ret += '\\node[state'
+                    ret += ', below of=' + str(getTag[findParent[p]][0]) + '-' + str(getTag[findParent[p]][1])
+                    ret += ', xshift='+ str(sibPos[sibNum]) +'mm*\\nodespace'
+                    ret += '] ('+ str(getTag[p][0]) + '-' + str(getTag[p][1]) + ') {' + str(p.getVal()) + '};\n'
+                    #draw the edges
+                    grandpa = findParent[p]
+                    ret += '\\draw'
+                    ret += '(' + str(getTag[grandpa][0]) + '-' + str(getTag[grandpa][1]) + ')' + \
+                    'edge[below] node{$ $} (' + str(getTag[p][0]) + '-' + str(getTag[p][1]) +');\n'
+                    sibNum = (sibNum + 1) % (numSiblings + 1)
+                else:
+                    ret += '\\node[state, initial] (0-0) {' + head.getVal() + '};\n'
+                if p:
+                    childLevel += p.getChildren()
+                
+            parentLevel = childLevel.copy()
+            childLevel.clear()
+            curLev += 1
             
         ret += '\\end{tikzpicture}\n' + \
         '\\end{document}'
+        ret = ret[:57] + '\\newcommand{\\numLevels}{'+ str(level)+'}\n' + ret[57:]
         return ret
-            
-    def levelOrder():
-        return 
-	    # levels = [[self.root]]
-		# curLevel = 1
-		# while any([n.left is not None or n.right is not None for n in levels[curLevel-1]]):
-		# 	maxWidth = 2 ** curLevel
-		# 	levels.append([])
-		# 	for parent in levels[curLevel - 1]:
-		# 		for child in [parent.left, parent.right]:
-		# 			if child is not None:
-		# 				levels[curLevel].append(child)
-		# 			else:
-		# 				levels[curLevel].append(Node('*'))
-		# 	curLevel += 1
-        #     ret = ''
-        #     curLevel -= 1
-        #     spaces = 1
-        #     while curLevel >= 0:
-        #         line = ''
-        #         if curLevel != len(levels) - 1:
-        #             for item in levels[curLevel]:
-        #                 line += ' '*(spaces) + '/' + ' '*(spaces) + '\\'
-        #         ret = '\n' + line + ret
-        #         line = ''
-        #         for item in levels[curLevel]:
-        #             line += ' '*(spaces*3//2) + str(item.value) + ' '*spaces
-        #         ret = '\n' + line + ret
-        #         curLevel -= 1
-        #         spaces *=2
-		#     return ret
         
 def main():
-    from tree import BST
-    lst = [5, 7, 8, 2, 3, 4, 1, 0]
-    myTree = BST(lst)
+    # from tree import BST
+    # lst = [5, 7, 8, 2, 3, 4, 1, 0 ,443, 5, 6, 7, 2]
+    # myTree = BST(lst)
+    # print(myTree)
+    from trie import Trie
+    myTree = Trie()
+    myTree.insert('ok')
+    myTree.insert('on')
+    myTree.insert('oklahoma')
+    myTree.insert('okanawa')
+    myTree.insert('optimism')
     dt = drawTree
     print(dt.run(myTree.root))
 
---
+++ trie.py
@@ -0,0 +1,38 @@
+from drawTree import PrintNode
+class TrieNode(PrintNode):
+    def __init__(self, ch='\$'):
+        self.children = {}
+        self.word = False
+        self.ch = ch
+    def getChildren(self):
+        return list(self.children.values())
+    def getVal(self):
+        return self.ch
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode(c)
+            curr = curr.children[c]
+        curr.word = True
+
+    def search(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.word
+
+    def startsWith(self, prefix):
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
